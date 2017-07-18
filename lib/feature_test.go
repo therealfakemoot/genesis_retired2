24a385d52d522c6a92611206708969d44cc260f6
Nick Dumas <drakonik@gmail.com>
2017-07-18 18:13:21 +0000

    Unit tests for Feature type.
    First pass at building out unit tests. Implemented a helper function or
    two for creating randomized Features, and started scaffolding out what I
    think I'd need for generating 'arbitrarily' deep/wide Feature Trees.

--- lib/feature_test.go
+++ lib/feature_test.go
@@ -1,9 +1,71 @@
 package genesis
 
 import (
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 )
+
+type TreeAxis struct {
+	Max int
+	Min int
+}
+
+type LocMapFunc func(p *Point) *Point
+
+func featureTree(depth TreeAxis, width TreeAxis, locMapFunc LocMapFunc) *Feature {
+	return randomLocFeature()
+}
+
+func randomLocFeature() *Feature {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	x := r.Int()
+	y := r.Int()
+	z := r.Int()
+
+	return dummyFeature(x, y, z)
+
+}
+
+func dummyFeature(x int, y int, z int) *Feature {
+	m := map[string]interface{}{
+		"x": x,
+		"y": y,
+		"z": z,
+	}
+
+	f, _ := NewFeature(m)
+	return f
+}
+
+func TestFeatureWalk(t *testing.T) {
+
+	var walkTests = []struct {
+		in  *Feature
+		out int
+	}{
+		{randomLocFeature(), 1},
+	}
+
+	ctx := &WalkCtx{}
+
+	for _, tt := range walkTests {
+		walkCount := 0
+
+		walkFunc := func(r *Feature, ctx WalkCtx) error {
+			walkCount += 1
+			return nil
+		}
+
+		tt.in.Walk(walkFunc, ctx)
+
+		if walkCount != 1 {
+			t.Errorf("Expected walkcount = 1, got %v", walkCount)
+		}
+	}
+
+}
 
 func TestFeatureMove(t *testing.T) {
 	locMap := make(map[string]interface{})
@@ -13,7 +75,4 @@ func TestFeatureMove(t *testing.T) {
 		t.Errorf("Expected %v, got %v", locMap, feature.LocMap)
 	}
 
-}
-
-func main() {
 }
