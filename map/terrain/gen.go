1410eb293e29cce24b23acf7754fcd602d5889c0
Nick Dumas <nicholas.dumas@homes.com>
2018-09-18 17:52:48 +0000

    Refactor to match Python implementation.

--- map/terrain/gen.go
+++ map/terrain/gen.go
@@ -1,7 +1,12 @@
 package genesis
 
 import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	l "github.com/therealfakemoot/genesis/log"
 	noise "github.com/therealfakemoot/genesis/noise"
+	Q "github.com/therealfakemoot/go-quantize"
 )
 
 // MapGen will allow for reuse and iterative tweaking of noise generation
@@ -18,19 +23,33 @@ func (mg *MapGen) Generate(x, y, sampleScale, thresholdScale float64) Map {
 	m := Map{}
 	m.Grid = Grid{X: int(x), Y: int(y), Z: 0}
 	points := make([][]float64, int(y))
-	for i := 0.0; i < y; i++ {
-		row := make([]float64, int(x))
-		points[int(i)] = row
-		// m.Points[int(i)] = row
-	}
-	m.Points = points
 
-	for xGen := 0.0; xGen < x; xGen++ {
-		for yGen := 0.0; yGen < y; yGen++ {
-			// m.Points[int(xGen)][int(yGen)] = float64(float32((mg.Noise.Eval3(xGen*sampleScale, yGen*sampleScale, 0) + 1) * thresholdScale))
-			m.Points[int(xGen)][int(yGen)] = mg.Noise.Eval3(xGen*sampleScale, yGen*sampleScale, 0)
-		}
+	d := Q.Domain{
+		Max:  viper.GetFloat64("Terrain.Domain.Max"),
+		Min:  viper.GetFloat64("Terrain.Domain.Min"),
+		Step: viper.GetFloat64("Terrain.Domain.Step"),
 	}
+
+	l.Term.WithFields(logrus.Fields{
+		"domain": fmt.Sprintf("%+v", d),
+	}).Debug("Domain")
+
+	for yGen := 0.0; yGen < y; yGen++ {
+		row := make([]float64, int(x))
+		for xGen := 0.0; xGen < x; xGen++ {
+			row[int(xGen)] = mg.Noise.Eval3(xGen*sampleScale, yGen*sampleScale, 0)
+		}
+		quantized := d.Quantize(row)
+
+		points[int(yGen)] = quantized
+
+		l.Term.WithFields(logrus.Fields{
+			"Raw Row":       row,
+			"Quantized Row": quantized,
+		}).Debug(fmt.Sprintf("Row %0.f", yGen))
+	}
+
+	m.Points = points
 
 	return m
 }
